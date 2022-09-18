ffcb1a4ea8d9709cc25310ded0804f9b624894aa
bingle-a81 <breath44@yandex.ru>
2022-09-18 10:26:12 +0300

    100

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -3,4 +3,20 @@ class SmartPhone:
         pass
 
 
-        
+class Point:
+    def __init__(self,pt):
+        self.pt=pt
+
+    @property
+    def pt(self):
+        return self.__pt
+
+    @pt.setter
+    def pt(self,pt):
+        if type(pt)==int:
+            self.__pt=pt
+
+a=Point(10)
+a.pt=100
+
+print(a.pt)
