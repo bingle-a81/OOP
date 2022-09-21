7a75126cba5c9d3386c5d99f2838f7ec84bae2e1
STUD <breath44@yande.ru>
2022-09-21 17:29:45 +0300

    1

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,20 +1,4 @@
-# import random
-# import string
-# def d(lst:list):
-#     a=len(lst)-1
-#     return lst[random.randint(0,a)]
+from functools import reduce
 
-# lst1=[i for i in map(int,string.digits)]
-# a=len(lst1)-1
-# b=[d(lst1) for i in lst1]
-# b2=[lst1[random.randint(0,a)] for i in lst1]
-# print(b)
-# print(b2)
- 
-# b3=list(zip(b,b2))
-# print(b3)
-
-s2=[1, 9, 8, 1, 4, 6, 2, 9, 8, 2]
-s1=[1, 4, 2, 0, 8, 9, 1, 6, 8, 8]
-s3=list(zip(s2,s1))
-print(s3)
+items = [10, 20, 30, 40, 50]
+print(reduce(lambda x,y: x + y, items))
