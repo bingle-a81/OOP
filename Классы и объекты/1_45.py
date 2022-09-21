b128c194202bdf45078ab7b88c59f671dca78534
bingle-a81 <breath44@yandex.ru>
2022-09-21 07:41:06 +0300

    2

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,7 +1,20 @@
-# days = [d +1 for d in range(31) if d % 7 < 5]
-# print(days)
+# import random
+# import string
+# def d(lst:list):
+#     a=len(lst)-1
+#     return lst[random.randint(0,a)]
 
-print(3%4)
+# lst1=[i for i in map(int,string.digits)]
+# a=len(lst1)-1
+# b=[d(lst1) for i in lst1]
+# b2=[lst1[random.randint(0,a)] for i in lst1]
+# print(b)
+# print(b2)
+ 
+# b3=list(zip(b,b2))
+# print(b3)
 
-for a,i in enumerate(range(1,32),1):
-    print(a,':',i%7)
+s2=[1, 9, 8, 1, 4, 6, 2, 9, 8, 2]
+s1=[1, 4, 2, 0, 8, 9, 1, 6, 8, 8]
+s3=list(zip(s2,s1))
+print(s3)
