660a49ab07f0d41abac7c896589459b637e76554
bingle-a81 <breath44@yandex.ru>
2022-09-22 08:45:15 +0300

    111

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,4 +1,23 @@
 from functools import reduce
 
-items = [10, 20, 30, 40, 50]
-print(reduce(lambda x,y: x + y, items))
+people = [{'имя': 'Маша', ' рост ': 160},
+    {'имя': 'Саша', ' рост ': 80},
+    {'name': 'Паша',' рост ': 80}]
+
+height_total = 0
+height_count = 0
+for person in people:
+    if ' рост ' in person:
+        height_total += person[' рост ']
+        height_count += 1
+
+if height_count > 0:
+    average_height = height_total / height_count
+
+print(average_height    )
+
+heights =list(map(lambda x: x[' рост '],filter(lambda e: ' рост ' in e, people)))
+print(heights)
+if len(heights) > 0:
+    average_height = sum(heights) / len(heights)
+print(average_height)
