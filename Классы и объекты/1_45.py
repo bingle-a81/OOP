03c4f2f3ee9720aff54cbc5f0bfcdd3fd038db55
STUD <breath44@yande.ru>
2022-09-26 09:56:59 +0300

    2

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,6 +1,23 @@
-class Fu:
-    _isinctace=None
-    def __new__(cls,*args,**kwargs) :
-        if cls._isinctace is None:
-            cls._isinctace=super().__new__(cls)
-        return cls._isinctace
+from functools import reduce
+
+people = [{'имя': 'Маша', ' рост ': 160},
+    {'имя': 'Саша', ' рост ': 80},
+    {'name': 'Паша',' рост ': 80}]
+
+height_total = 0
+height_count = 0
+for person in people:
+    if ' рост ' in person:
+        height_total += person[' рост ']
+        height_count += 1
+
+if height_count > 0:
+    average_height = height_total / height_count
+
+print(average_height    )
+
+heights =list(map(lambda x: x[' рост '],filter(lambda e: ' рост ' in e, people)))
+print(heights)
+if len(heights) > 0:
+    average_height = sum(heights) / len(heights)
+print(average_height)
