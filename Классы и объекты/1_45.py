9dfc40ea687c0bd6a4e447d0c3468000e0f85658
bingle-a81 <breath44@yandex.ru>
2022-09-28 08:21:32 +0300

    3

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,15 +1,28 @@
-class Decorator:
+class HandlerGET:
     def __init__(self, func):
-        self.__func = func
+        self.infunction = func
+    
+    def __call__(self, request, *args, **kwargs):
+        item = request.get("method","GET")
+        if not item or item != "GET":
+            return None
+        return self.get(self.infunction, request)
 
-    def __call__(self, x, y=1):
-        return self.__func(x) + y
+    def get(self, func, request, *args, **kwargs):
+        print(func(request))
+        return f"GET: {func(request)}"
 
 
-@Decorator
-def my_func(x):
-    return x
+@HandlerGET
+def index(request):
+    return "главная страница сайта"
 
-print(type(my_func))
-a = my_func(10,11)  # Попробуйте добавить второй аргумент
-print(a)
+res = index({"method": "GET"})
+assert res == "GET: главная страница сайта", "декорированная функция вернула неверные данные"
+res = index({"method": "POST"})
+assert res is None, "декорированная функция вернула неверные данные"
+res = index({"method": "POST2"})
+assert res is None, "декорированная функция вернула неверные данные"
+
+res = index({})
+assert res == "GET: главная страница сайта", "декорированная функция вернула неверные данные"
