0ac7395330831b11add63fbafc7472bfb1638720
bingle-a81 <breath44@yandex.ru>
2022-10-03 21:58:06 +0300

    3

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,49 +1,15 @@
-class Ingredient:
-    def  __init__(self,name,volume,measure,):
-        self.name=name
-        self.volume=volume
-        self.measure=measure
+class Way:
+    def __init__(self, length):
+        self.length = length
 
-    def __str__(self):
-        return ( f'{self.name}: {self.volume}, {self.measure}')
-
-class Recipe:
-    def __init__(self,*args):
-        self.rec=list(args) if args else []
-
-    def add_ingredient(self,ing:Ingredient):
-        self.rec.append(ing)
-
-    def remove_ingredient(self,ing:Ingredient):
-        self.rec.remove(ing)
-
-    def get_ingredients(self):
-        return self.rec
-
-    def __len__(self):
-        return len(self.rec)
+    def __add__(self, other):
+        return Way(self.length + other.length)
 
 
 
-i1 = Ingredient("Соль", 1, "столовая ложка")
-i2 = Ingredient("Мука", 1, "кг")
-i3 = Ingredient("Мясо баранины", 10, "кг")
-i4 = Ingredient("Масло", 100, "гр")
-recipe = Recipe(i1, i2, i3)
-recipe.add_ingredient(i4)
-recipe.remove_ingredient(i3)
+w1 = Way(5)
+w2 = Way(10)
 
-assert len(recipe) == 3, "функция len вернула неверное значение"
-lst = recipe.get_ingredients()
-for x in lst:
-    assert isinstance(x, Ingredient), "в списке рецептов должны быть только объекты класса Ingredient"
-    assert hasattr(x, 'name') and hasattr(x, 'volume') and hasattr(x, 'measure'), "объект класса Ingredient должен иметь атрибуты: name, volume, measure"
-
-
-assert str(i4) == "Масло: 100, гр", "метод __str__ вернул неверное значение"
-
-i4 = Ingredient("Масло", 120, "гр")
-recipe.add_ingredient(i4)
-assert len(recipe) == 4, "функция len вернула неверное значение"
-
-
+w = w1 + w2
+w1 += w2
+w = w1 + w2 + w1
