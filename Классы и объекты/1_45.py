110aa74e6b75623b0db6c9b790ba7e855d813b9a
STUD <breath44@yande.ru>
2022-10-03 13:08:53 +0300

    3

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,34 +1,28 @@
-def counter(start=0):
-    def step():
-        nonlocal start
-        start+=1
-        return start
-    return step
+class Clock:
+    def __init__(self, hours, minutes, seconds):
+        self._hours = hours
+        self._minutes = minutes
+        self._seconds = seconds
 
-c1=counter()
-print(c1())
-print(c1())
+    def get_time(self):
+        return self._hours * 3600 + self._minutes * 60 + self._seconds
 
 
-class Deco:
-    def __init__(self,param):
-        self.param=param
+class DeltaClock:
+    def __init__(self, clock1: Clock, clock2: Clock):
+        self._clock1 = clock1
+        self._clock2 = clock2
 
-    def __call__(self,func ):
-        self.func=func
-        def wrapper(*args, **kwds):
-            result=self.func(list(map(str,(i+self.param for i in args[0]))))
-            return result
-        return wrapper
+    def __len__(self):
+        d = self._clock1.get_time() - self._clock2.get_time()
+        return d if d > 0 else 0
 
-@Deco(3)
-def ff(x):
-    print(x)
-
-ff(range(10))
-
-# print(a)
-
-
-            
-
+    def __str__(self):
+        d = self.__len__()
+        h = d // 3600
+        m = d % 3600 // 60
+        s = d % 3600 % 60
+        return f'{h:02}: {m:02}: {s:02}'
+dt = DeltaClock(Clock(3, 45, 15), Clock(1, 30, 0))
+print(dt) # 01: 30: 00
+len_dt = len(dt) # 5400
