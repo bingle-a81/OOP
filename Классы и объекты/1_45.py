1c54fb61325cabf186ef0b808d2731f00fe1e4c3
STUD <breath44@yande.ru>
2022-10-07 17:44:41 +0300

    4

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,12 +1,67 @@
-class Box:
-    def __init__(self):
-        self.boxes=[]
+class CentralBank:
+    rates = {'rub': 72.5, 'dollar': 1.0, 'euro': 1.15}
 
-    def add_thing(self, obj):
-        self.boxes.append(obj)
+    def __new__(cls, *args, **kwargs):
+        return
 
-    def get_things(self):
-        return self.boxes
 
-class Thing:
-    def __init__(self):
+    @classmethod
+    def register(cls, money):
+        money.cb=cls
+
+
+class Money:
+    type_money=None
+    EPS=0.1
+
+    def __init__(self,volume=0):
+        self.__volume=volume
+        self.__cb=None
+
+    @property
+    def cb(self):
+        return self.__cb
+    @cb.setter
+    def cb(self,obj):
+        self.__cb=obj
+
+    @property
+    def volume(self):
+        return self.__volume
+    @volume.setter
+    def volume(self,volume):
+        if type(volume) in (int,float):
+            self.__volume=volume
+
+    def get_volumes(self,other):
+        if self.cb is None:
+            raise ValueError("Неизвестен курс валют.")
+
+        if self.type_money is None:
+            raise ValueError("")
+
+        v1 = self.volume / self.cb.rates[self.type_money]
+        v2 = other.volume / other.cb.rates[other.type_money]
+
+        return v1,v2
+
+    def __eq__(self, other):
+        v1,v2=self.get_volumes(other)
+        return abs(v1-v2)<self.EPS
+
+    def __lt__(self, other):
+        v1, v2 = self.get_volumes(other)
+        return v1<v2
+
+    def __le__(self, other):
+        v1, v2 = self.get_volumes(other)
+        return v1<=v2
+
+class MoneyR(Money):
+    type_money = 'rub'
+
+class MoneyD(Money):
+    type_money = 'dollar'
+
+class MoneyE(Money):
+    type_money = 'euro'
