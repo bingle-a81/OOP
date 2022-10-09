f607b2e62280362dc524ce0ccb00e0ca4b6b10d6
bingle-a81 <breath44@yandex.ru>
2022-10-09 10:38:37 +0300

    5

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,67 +1,23 @@
-class CentralBank:
-    rates = {'rub': 72.5, 'dollar': 1.0, 'euro': 1.15}
-
-    def __new__(cls, *args, **kwargs):
-        return
+class Strin:
+    def __init__(self,st) :
+        self.st=st
 
 
-    @classmethod
-    def register(cls, money):
-        money.cb=cls
+
+s="""   Унылая пора! Очей очарованье!
+        Приятна мне твоя прощальная краса 
+        — Люблю я пышное природы увяданье,
+        В багрец и в золото одетые леса,
+        В их сенях ветра шум и свежее дыханье,
+        И мглой волнистою покрыты небеса,
+        И редкий солнца луч, и первые морозы,
+        И отдаленные седой зимы угрозы.
+"""
 
 
-class Money:
-    type_money=None
-    EPS=0.1
+textus=[Strin(x.split()) for x in s.splitlines()]
 
-    def __init__(self,volume=0):
-        self.__volume=volume
-        self.__cb=None
+print(textus)
 
-    @property
-    def cb(self):
-        return self.__cb
-    @cb.setter
-    def cb(self,obj):
-        self.__cb=obj
-
-    @property
-    def volume(self):
-        return self.__volume
-    @volume.setter
-    def volume(self,volume):
-        if type(volume) in (int,float):
-            self.__volume=volume
-
-    def get_volumes(self,other):
-        if self.cb is None:
-            raise ValueError("Неизвестен курс валют.")
-
-        if self.type_money is None:
-            raise ValueError("")
-
-        v1 = self.volume / self.cb.rates[self.type_money]
-        v2 = other.volume / other.cb.rates[other.type_money]
-
-        return v1,v2
-
-    def __eq__(self, other):
-        v1,v2=self.get_volumes(other)
-        return abs(v1-v2)<self.EPS
-
-    def __lt__(self, other):
-        v1, v2 = self.get_volumes(other)
-        return v1<v2
-
-    def __le__(self, other):
-        v1, v2 = self.get_volumes(other)
-        return v1<=v2
-
-class MoneyR(Money):
-    type_money = 'rub'
-
-class MoneyD(Money):
-    type_money = 'dollar'
-
-class MoneyE(Money):
-    type_money = 'euro'
+for i in textus:
+    print(f'{i.st}')
