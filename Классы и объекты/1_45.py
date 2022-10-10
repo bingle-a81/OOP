bd473d93a1fa3c21d9dd628c3415804daabbae32
bingle-a81 <breath44@yandex.ru>
2022-10-10 21:39:25 +0300

    1

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,23 +1,46 @@
-class Strin:
-    def __init__(self,st) :
-        self.st=st
+class Deco:
+    def __set_name__(self,owner,name):
+        self.name=f'_{owner.__name__}__{name}'
+
+    def __get__(self,owner,objtype):
+        return getattr(owner,self.name)
+    
+    def __set__(self,instance,value):
+        setattr(instance,self.name,value)
 
 
-
-s="""   Унылая пора! Очей очарованье!
-        Приятна мне твоя прощальная краса 
-        — Люблю я пышное природы увяданье,
-        В багрец и в золото одетые леса,
-        В их сенях ветра шум и свежее дыханье,
-        И мглой волнистою покрыты небеса,
-        И редкий солнца луч, и первые морозы,
-        И отдаленные седой зимы угрозы.
-"""
+class Don:
+    start=Deco()
+    end=Deco()
+    def __init__(self,start=None,end=None) :
+        self.start=start
+        self.end=end
 
 
-textus=[Strin(x.split()) for x in s.splitlines()]
+class Lst:
+    def __init__(self):
+        self.obj_nachalo=None
+        self.obj_konec=None
 
-print(textus)
+    def add(self,obj:Don):
+        if self.obj_konec:
+            self.obj_konec=obj.start
+        obj.end=self.obj_konec
+        self.obj_konec=obj
+        if not self.obj_nachalo:
+            self.obj_nachalo=obj
+        
 
-for i in textus:
-    print(f'{i.st}')
+a1=Don()
+a2=Don()
+a3=Don()
+a4=Don()
+l=Lst()
+l.add(a1)
+print(l.n,l.f)
+l.add(a2)
+print(l.n,l.f)
+l.add(a3)
+print(l.n,l.f)
+l.add(a4)
+print(l.n,l.f)
