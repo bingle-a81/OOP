f4c063ba40bf6ea71f23159935fa2d84450ea5ec
bingle-a81 <breath44@yandex.ru>
2022-10-23 11:11:03 +0300

    1

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,28 +1,55 @@
-class Integer:
-    def __init__(self,start_value):
-        self.start_value=start_value
-        self.__value=value
+class Cell:
+    def __init__(self) -> None:
+        self.is_free=True
+        self.value=0
 
-    @property
-    def value(self):
-        return self.__value
-    @value.setter
-    def value(self,v):
-        if type(v) != int:
-            raise ValueError('должно быть целое число')
-        self.__value=v
+    def __bool__(self):
+        return self.is_free
 
-class Array:
-    def __init__(self,max_length, cell:Integer):
-        self.max_length=max_length
-        self.cell=cell
-        self.ar=[]
+class TicTacToe:
+    def __init__(self) -> None:
+        self.pole=tuple(tuple(Cell() for i in range(3)) for i in range(3))
+
+    def clear(self):
+        for row in self.pole:
+            for cell in row:
+                cell.is_free=True
+                cell.value=0
 
     def __check(self,v):
-        if type(v) != int or not (0<v<self.max_length):
-            raise IndexError('неверный индекс для доступа к элементам массива')
+        if type(v)!=tuple or len(v) !=2:
+            raise IndexError('неверный индекс клетки')
+        if any(not(0<=x<=3) for x in v if type(x) !=slice):
+            raise IndexError('неверный индекс клетки')
+    
+    def __setitem__(self,key,value):
+        self.__check(key)
+        r,c=key
+        if self.pole[r][c]:
+            self.pole[r][c].value=value
+            self.pole[r][c].is_free=False
+        else:
+            raise ValueError('клетка уже занята')
+    
+    def __getitem__(self,item):
+        self.__check(item)
+        r,c=item
+        if type(r)==slice:
+            return tuple(self.pole[x][c].value for x in range(3))
+        if type(c)==slice:
+            return tuple(self.pole[r][x].value for x in range(3))
+        return self.pole[r][c].value
 
-
-    def __getitem__(self, item):
-        if self.__check(item):
-            return self.ar.
+game = TicTacToe()
+game.clear()
+game[0, 0] = 1
+game[1, 0] = 2
+# формируется поле:
+# 1 0 0
+# 2 0 0
+# 0 0 0
+game[3, 2] = 2 # генерируется исключение IndexError
+if game[0, 0] == 0:
+    game[0, 0] = 2
+v1 = game[0, :]  # 1, 0, 0
+v2 = game[:, 0]  # 1, 2, 0
