aa07db07a56c524b121a7a1c337012033dc863b9
Al <breath44@yandex.ru>
2022-10-28 20:37:26 +0300

    10

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,55 +1,14 @@
-class Cell:
-    def __init__(self) -> None:
-        self.is_free=True
-        self.value=0
+class Bot:
+    def __init__(self,*args) :
+        self.coords=list(*args)
 
-    def __bool__(self):
-        return self.is_free
-
-class TicTacToe:
-    def __init__(self) -> None:
-        self.pole=tuple(tuple(Cell() for i in range(3)) for i in range(3))
-
-    def clear(self):
-        for row in self.pole:
-            for cell in row:
-                cell.is_free=True
-                cell.value=0
-
-    def __check(self,v):
-        if type(v)!=tuple or len(v) !=2:
-            raise IndexError('неверный индекс клетки')
-        if any(not(0<=x<=3) for x in v if type(x) !=slice):
-            raise IndexError('неверный индекс клетки')
-    
-    def __setitem__(self,key,value):
-        self.__check(key)
-        r,c=key
-        if self.pole[r][c]:
-            self.pole[r][c].value=value
-            self.pole[r][c].is_free=False
-        else:
-            raise ValueError('клетка уже занята')
-    
     def __getitem__(self,item):
-        self.__check(item)
-        r,c=item
-        if type(r)==slice:
-            return tuple(self.pole[x][c].value for x in range(3))
-        if type(c)==slice:
-            return tuple(self.pole[r][x].value for x in range(3))
-        return self.pole[r][c].value
+        return self.coords[item]
 
-game = TicTacToe()
-game.clear()
-game[0, 0] = 1
-game[1, 0] = 2
-# формируется поле:
-# 1 0 0
-# 2 0 0
-# 0 0 0
-game[2, 2] = 2 # генерируется исключение IndexError
-if game[0, 0] == 0:
-    game[0, 0] = 2
-v1 = game[0, :]  # 1, 0, 0
-v2 = game[:, 0]  # 1, 2, 0
+    def __setitem__(self,key,value):
+        self.coords[key]=value
+
+a=list(range(10))
+d=Bot(a)
+d[5]=10000
+print (d.coords)
