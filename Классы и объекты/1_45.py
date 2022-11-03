54f14745d16f0c5901998aafa770e6e8ae8153cd
Al <breath44@yandex.ru>
2022-11-03 08:30:08 +0300

    2

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,40 +1,15 @@
-class Cell:
-    def __init__(self) -> None:
-        self.is_free=True
-        self.value=0
-
-    def __bool__(self):
-        return self.is_free
-
-    def __repr__(self) -> str:
-        return f'{self.value}'
+letters_amazon = '''
+We spent several years building our own database engine,
+Amazon Aurora, a fully-managed MySQL and PostgreSQL-compatible
+service with the same or better durability and availability as
+the commercial engines, but at one-tenth of the cost. We were
+not surprised when this worked.
+'''
 
 
+fiind = lambda x, q: x[x.find(q):x.find(q)] if q in x else -1
 
-class TicTacToe:
-    def __init__(self) -> None:
-        self.__n=3
-        self.pole=tuple(tuple(Cell() for row in range(self.__n)) for col in range(self.__n))
+f=[letters_amazon.find('SQL'),letters_amazon.find('SQL')]
+print(f)
 
-    def clear(self):
-        for row in self.pole:
-            for i in row:
-                i.is_free=True
-                i.value=0
-
-    def __repr__(self) -> str:
-        for i in self.pole: 
-            print(' '.join(list(map(str, i))))
-
-
-
-# p=TicTacToe()
-# print(TicTacToe())
-
-pole=tuple(tuple('0' for row in range(2)) for col in range(2))
-print(len(pole))
-
-print (list(pole[i-1][i] for i in range(1,len(pole))))
-
-# a=(0,0,0,0,)
-# print(*a)
+# print(fiind(letters_amazon, 'SQL'))
