68fbe04d0ccb79c829bd6b2661efa91dc4393dd2
Al <breath44@yandex.ru>
2022-11-11 05:14:47 +0300

    Record

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,15 +1,9 @@
-class Record:
-    def __init__(self,**kwargs) -> None:
-        self.__dict=kwargs
-        self.__len=len(kwargs)
-        self.__dict_list=[*self.__dict.keys()]
+class RadiusVector:
+    def __init__(self,*args) -> None:
+        self.coords=list(args) if args else []
 
     def __getitem__(self,item):
-        if type(item)!=int or not (-self.__len<=item<=self.__len):
-            raise IndexError('генерируется исключение IndexError')
-        return getattr(self,self.__dict_list[item])
+        return tuple(self.coords[item]) if type(item)== slice else self.coords[item]
 
     def __setitem__(self,key,value):
-        if type(key)!=int or not (-self.__len<=key<=self.__len):
-            raise IndexError('генерируется исключение IndexError')
-        return setattr(self,self.__dict_list[key],value)
+        self.coords[key]=value
