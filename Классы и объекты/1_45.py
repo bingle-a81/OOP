d927555deb277772da2cf0491c43dbb1d7429cc1
Al <breath44@yandex.ru>
2022-11-11 05:36:43 +0300

    clock

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,9 +1,29 @@
-class RadiusVector:
-    def __init__(self,*args) -> None:
-        self.coords=list(args) if args else []
+class Clock:
+    def __init__(self,h,m,s) -> None:
+        self.hour=h
+        self.minute=m
+        self.second=s
 
-    def __getitem__(self,item):
-        return tuple(self.coords[item]) if type(item)== slice else self.coords[item]
+    def get_time(self):
+        return sum(x for x in [self.hour*3600,self.minute*60,self.second])
 
-    def __setitem__(self,key,value):
-        self.coords[key]=value
+
+class DeltaClock:
+    def __init__(self,cl1:Clock,cl2:Clock) -> None:
+        self.clock1=cl1
+        self.clock2=cl2
+
+    def __len__(self):
+        res=self.clock1.get_time-self.clock2.get_time
+        return res if res>0 else 0
+
+    def __repr__(self) -> str:
+        d=self.__len__()
+        h=d//3600
+        m=d%3600//60
+        s=d%3600%60
+        return f'{h:02}:{m:02}:{s:02}'
+
+
+c=Clock(1,2,3)
+print(c.get_time())
