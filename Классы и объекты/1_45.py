64e1ab4fc750b3af01765c6a6764ee13cc5f2bae
STUD <breath44@yande.ru>
2022-11-22 17:14:46 +0300

    4

--- Классы и объекты/1_45.py
+++ Классы и объекты/1_45.py
@@ -1,15 +1,6 @@
-r,c=5,5
-matrix=[[0 for x in range(c)] for y in range(r)]
+a=('a','b','c')
+b=('1','2','3')
+c=list(zip(a,b))
+print(c)
 
-dx,dy,x,y=0,1,0,0
 
-for i in range(1,r*c+1):
-    matrix[x][y]=i
-    if matrix[(dx+x)%r][(dy+y)%c]:
-        dx,dy=dy,-dx
-    x+=dx
-    y+=dy
-
-for line in matrix:
-    print(*(f'{i:<3}' for i in line), sep='')
-    
